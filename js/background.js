8d2748157afbc3dc17fbdfdb6a3430e58697b492
aemmanouilidis <aemmanouilidis@intelligen.com>
2023-02-03 22:35:36 +0200

    better formating

--- js/background.js
+++ js/background.js
@@ -3,39 +3,39 @@
 //When the tab bar icon is clicked this code runs. It sets the state of the extension.
 chrome.action.onClicked.addListener(async (tab) => {
 
-    chrome.storage.local.get('state', async function(data) {
+  chrome.storage.local.get('state', async function (data) {
 
-      if(data.state == 'on') {
-        //Save the state of the extension.
-        chrome.action.setBadgeText({text:"On"});
-        chrome.storage.local.set({state: 'off'});
+    if (data.state == 'on') {
+      //Save the state of the extension.
+      chrome.action.setBadgeText({ text: "On" });
+      chrome.storage.local.set({ state: 'off' });
 
-        try{
+      try {
         await chrome.scripting.insertCSS({
-            target: {
-                tabId: tab.id,
-            },
-            files: ["css/styles.css"],
+          target: {
+            tabId: tab.id,
+          },
+          files: ["css/styles.css"],
         });
-      }catch(err) {
+      } catch (err) {
         console.error(`failed to insert CSS: ${err}`);
       }
-      } else {
-        //Save the state of the extension.
-        chrome.action.setBadgeText({text:"Off"});
-        chrome.storage.local.set({state: 'on'})
+    } else {
+      //Save the state of the extension.
+      chrome.action.setBadgeText({ text: "Off" });
+      chrome.storage.local.set({ state: 'on' })
 
-        try{
+      try {
         await chrome.scripting.removeCSS({
-            target: {
-                tabId: tab.id,
-            },
-            files: ["css/styles.css"],
+          target: {
+            tabId: tab.id,
+          },
+          files: ["css/styles.css"],
         });
-      }catch(err) {
+      } catch (err) {
         console.error(`failed to remove CSS: ${err}`);
       }
-      }
-      
-    });
+    }
+
   });
+});
